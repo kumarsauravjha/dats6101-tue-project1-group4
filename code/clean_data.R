16f0ee17daa1800ed5b5b178d362f0c33d898320
dhanush-bhargav <dhanushsbhargav@gmail.com>
2023-09-30 18:59:29 -0400

    clean up dataset

---
+++ code/clean_data.R
@@ -0,0 +1,12 @@
+nfl_data = read.csv("dataset/nfl_players.csv")
+
+nfl_data = nfl_data[, c("position_group", "position", "height", "weight", "years_of_experience", "team_abbr", "entry_year", "rookie_year")]
+
+nfl_data$position_group = as.factor(nfl_data$position_group)
+nfl_data$position = as.factor(nfl_data$position)
+nfl_data$team_abbr = as.factor(nfl_data$team_abbr)
+
+nfl_data_clean = na.omit(nfl_data)
+nfl_data_clean = subset(nfl_data_clean, nfl_data_clean$entry_year >= 2000)
+
+write.csv(nfl_data_clean, "dataset/nfl_players_clean.csv")
