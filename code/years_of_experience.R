b438640e0ed0bb1e26a56bed81e30ee76ee43bc9
dhanush-bhargav <dhanushsbhargav@gmail.com>
2023-10-12 13:25:00 -0400

    add color to plots

--- code/years_of_experience.R
+++ code/years_of_experience.R
@@ -6,10 +6,22 @@ nfl_data$position_group = as.factor(nfl_data$position_group)
 nfl_data$position = as.factor(nfl_data$position)
 nfl_data$team_abbr = as.factor(nfl_data$team_abbr)
 
-plt1 <- ggplot(nfl_data) + geom_histogram(aes(x=years_of_experience), binwidth = 1)
-plt2 <- ggplot(nfl_data) + geom_boxplot(aes(y=years_of_experience))
-plt3 <- ggplot(nfl_data) + geom_boxplot(aes(x=position_group, y=years_of_experience))
-print(summary(nfl_data$years_of_experience))
+plt1 <- ggplot(nfl_data) +
+  geom_histogram(aes(x=years_of_experience), color="black", fill="yellow", binwidth = 1, show.legend = F) +
+  labs(title = "Histogram plot of years of experience for all players") +
+  labs(x = "Years of Experience", y="Number of players")
+
+plt2 <- ggplot(nfl_data) +
+  geom_boxplot(aes(y=years_of_experience), fill="yellow", show.legend = F) +
+  labs(title = "Boxplot of years of experience for all players") +
+  labs(y = "Years of experience")
+
+plt3 <- ggplot(nfl_data) +
+geom_boxplot(aes(x=position_group, y=years_of_experience, color=position_group), show.legend = F) +
+labs(title = "Boxplot of years of experience for each position group") +
+labs(x = "Position Group", y = "Years of experience")
+
+
 print(plt1)
 print(plt2)
 print(plt3)
