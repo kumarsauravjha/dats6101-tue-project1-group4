d666cd48e8cf54419ab4fb9e9cb12405f2b67340
vasettiv <vinay.vasetti@gwu.edu>
2023-10-11 21:24:25 -0400

    Vinay_EDA

--- dataset/Scatterplot.R
+++
@@ -1,8 +0,0 @@
-
-NFL_data <- read.csv("nfl_players_clean.csv")
-summary(NFL_data)
-library(ggplot2)
-scatterplot<-ggplot(NFL_data, aes(x = height, y = weight, color = position)) +
-  geom_point() + labs(x = "Height", y = "Weight") + ggtitle("Scatter Plot of Height vs. Weight by Position") +
-  theme_minimal()
-print(scatterplot)
