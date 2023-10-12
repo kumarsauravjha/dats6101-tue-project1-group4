592a76df693fcc80da1d46603d60c131ba81f949
dhanush-bhargav <dhanushsbhargav@gmail.com>
2023-10-11 22:26:21 -0400

    add boxplot

--- code/years_of_experience.R
+++ code/years_of_experience.R
@@ -1,2 +1,10 @@
+library(ggplot2)
+
 nfl_data = read.csv("dataset/nfl_players_clean.csv")
 
+nfl_data$position_group = as.factor(nfl_data$position_group)
+nfl_data$position = as.factor(nfl_data$position)
+nfl_data$team_abbr = as.factor(nfl_data$team_abbr)
+
+plt <- ggplot(nfl_data) + geom_boxplot(aes(x=position_group, y=years_of_experience))
+print(plt)
