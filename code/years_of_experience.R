6b139e22cc6979edae9ba8f6136b8baadc439098
dhanush-bhargav <dhanushsbhargav@gmail.com>
2023-10-13 20:51:22 -0400

    anova tests

--- code/years_of_experience.R
+++ code/years_of_experience.R
@@ -1,4 +1,5 @@
 library(ggplot2)
+library(ezids)
 
 #Read data
 nfl_data = read.csv("dataset/nfl_players_clean.csv")
@@ -22,18 +23,39 @@ plt2 <- ggplot(nfl_data) +
 
 #Boxplot by position
 plt3 <- ggplot(nfl_data) +
-geom_boxplot(aes(x=position_group, y=years_of_experience, color=position_group), show.legend = F) +
-labs(title = "Boxplot of years of experience for each position group") +
-labs(x = "Position Group", y = "Years of experience")
+  geom_boxplot(aes(x=position_group, y=years_of_experience, color=position_group), show.legend = F) +
+  labs(title = "Boxplot of years of experience for each position group") +
+  labs(x = "Position Group", y = "Years of experience")
 
+#Boxplot by team
+plt4 <- ggplot(nfl_data) +
+  geom_boxplot(aes(x=team_abbr, y=years_of_experience, color=team_abbr), show.legend = F) +
+  labs(title = "Boxplot of years of experience for each team") +
+  labs(x = "Team", y = "Years of experience")
 
 print(plt1)
 print(plt2)
 print(plt3)
+print(plt4)
 
+# Take subset of players with highest median
 dl_players <- subset(nfl_data, nfl_data$position_group == "DL")
-qb_players <- subset(nfl_data, nfl_data$position_group == "QB")
-db_players <- subset(nfl_data, nfl_data$position_group == "DB")
-print(summary(dl_players$years_of_experience))
-print(summary(qb_players$years_of_experience))
-print(summary(db_players))
+# print(summary(dl_players))
+
+# Take subset of players with lowest median
+ol_players <- subset(nfl_data, nfl_data$position_group == "OL")
+# print(summary(ol_players))
+
+
+#Hypothesis tests
+#Position group vs YOE
+pos_yoe <- aov(years_of_experience ~ position_group, nfl_data)
+print(xkabledply(pos_yoe))
+
+pos_yoe_tukey <- TukeyHSD(pos_yoe)
+print(pos_yoe_tukey)
+
+#Team vs YOE
+team_yoe <- aov(years_of_experience ~ team_abbr, nfl_data)
+print(xkabledply(team_yoe))
+
