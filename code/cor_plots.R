c86ba937779527791288d85117ba89aa61a67858
pavanichella12 <pavani.chella@gwu.edu>
2023-11-26 16:08:27 -0500

    Update cor_plots.R

--- code/cor_plots.R
+++ code/cor_plots.R
@@ -1,4 +1,7 @@
 
+
+nfl_players_clean=read.csv("dataset/nfl_players_clean.csv")
+
 #correalation plots for position group vs ht and wt
 
 # Create a box plot for weight and position group
@@ -12,55 +15,53 @@ box_ht<-boxplot(height ~ position_group, data = nfl_players_clean, col = "blue",
 print(box_ht)
 
 
-# Using the same data as above
+
 library(ggplot2)
 
 # Create a violin plot
-voilon_wt<-ggplot(nfl_data_clean, aes(x = weight, y = position_group, fill = weight)) +
+voilon_wt<-ggplot(nfl_players_clean, aes(x = weight, y = position_group, fill = weight)) +
   geom_violin() +
   labs(title = "Violin Plot of weight and position group")
 print(voilon_wt)
 
-voilon_ht<-ggplot(nfl_data_clean, aes(x = height, y = position_group, fill = height)) +
+voilon_ht<-ggplot(nfl_players_clean, aes(x = height, y = position_group, fill = height)) +
   geom_violin() +
   labs(title = "Violin Plot of height and position group")
 print(voilon_ht)
 
 
 
-# Using the same data as above
-scatter_ht<-ggplot(nfl_data_clean, aes(x = position_group, y = height, color = position_group)) +
+scatter_ht<-ggplot(nfl_players_clean, aes(x = position_group, y = height, color = position_group)) +
   geom_jitter() +
   labs(title = "Jittered Scatter Plot of position group by height")
 print(scatter_ht)
 
 
-scatter_wt<-ggplot(nfl_data_clean, aes(x = position_group, y = weight, color = position_group)) +
+scatter_wt<-ggplot(nfl_players_clean, aes(x = position_group, y = weight, color = position_group)) +
   geom_jitter() +
   labs(title = "Jittered Scatter Plot of position group by weight")
 print(scatter_wt)
 
 
-# Using the same data as above
-strip_wt<-ggplot(nfl_data_clean, aes(x = position_group, y = weight, color = position_group)) +
+strip_wt<-ggplot(nfl_players_clean, aes(x = position_group, y = weight, color = position_group)) +
   geom_point(position = position_jitter(width = 0.2), size = 3) +
   labs(title = "Strip Chart of position group by weight")
 print(strip_wt)
 
-strip_ht<-ggplot(nfl_data_clean, aes(x = position_group, y = height, color = position_group)) +
+strip_ht<-ggplot(nfl_players_clean, aes(x = position_group, y = height, color = position_group)) +
   geom_point(position = position_jitter(width = 0.2), size = 3) +
   labs(title = "Strip Chart of position group by height")
 print(strip_ht)
 
 
-# Using the same data as above
-faceted_wt<-ggplot(nfl_data_clean, aes(x = position_group, y = weight, color = position_group)) +
+
+faceted_wt<-ggplot(nfl_players_clean, aes(x = position_group, y = weight, color = position_group)) +
   geom_point() +
   facet_wrap(~position_group) +
   labs(title = "Faceted Scatter Plots of position group by weight")
 print(faceted_wt)
 
-faceted_ht<-ggplot(nfl_data_clean, aes(x = position_group, y = height, color = position_group)) +
+faceted_ht<-ggplot(nfl_players_clean, aes(x = position_group, y = height, color = position_group)) +
   geom_point() +
   facet_wrap(~position_group) +
   labs(title = "Faceted Scatter Plots of position group by height")
