fe061c24748de8d6ed5d4e522e1417ee1beab154
dhanush-bhargav <dhanushsbhargav@gmail.com>
2023-12-01 15:40:44 -0500

    KNN

---
+++ code/KNearestNeighbors.R
@@ -0,0 +1,28 @@
+library(caret)
+
+nfl_players_clean <- read.csv("dataset/nfl_players_clean.csv")
+
+nfl_players_clean <- subset(nfl_players_clean, nfl_players_clean$position_group != "SPEC")
+nfl_players_clean$position_group <- factor(nfl_players_clean$position_group)
+
+data <- nfl_players_clean[c("weight", "height", "position_group")]
+
+train_index <- createDataPartition(data$position_group, times=1, p=0.8, list=F)
+
+train_data <- data[train_index,]
+test_data <- data[-train_index,]
+
+preProcValues <- preProcess(train_data, method=c("center", "scale"))
+train_data <- predict(preProcValues, train_data)
+test_data <- predict(preProcValues, test_data)
+
+knn_model <- train(position_group~., data=train_data, method="knn",
+                    trControl=trainControl(method="cv"),
+                    tuneGrid=data.frame(k=c(20,40,50,100, 200, 250)))
+
+
+best_knn <- knn3(position_group ~., data=train_data, k=knn_model$bestTune$k)
+
+preds <- predict(best_knn, test_data, type="class")
+cm <- confusionMatrix(preds, test_data$position_group)
+print(cm)
