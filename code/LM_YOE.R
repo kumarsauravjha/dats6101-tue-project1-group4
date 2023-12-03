773ff403a6e39a746a22db4f8b51e79559760f03
kumarsauravjha <kumarsaurav.jha@gwu.edu>
2023-12-03 15:55:48 -0500

    Update LM_YOE.R
    Addition of interaction term and Random Forest

--- code/LM_YOE.R
+++ code/LM_YOE.R
@@ -77,3 +77,64 @@ rmse <- sqrt(mean((predictions - test_data$years_of_experience)^2))
 # Print the RMSE
 cat("Root Mean Squared Error (RMSE):", rmse, "\n")
 
+---------------------------------------------------
+  
+# Create interaction term between height and weight for the entire dataset
+nfl_clean$height_weight_interaction <- nfl_clean$height * nfl_clean$weight
+
+# Split the dataset into training (80%) and testing (20%) sets
+#set.seed(123)  # for reproducibility
+#train_indices <- sample(1:nrow(nfl_clean), 0.8 * nrow(nfl_clean))
+train_data1 <- nfl_clean[train_indices, ]
+test_data1 <- nfl_clean[-train_indices, ]
+
+# Build the model with interaction term on the training set
+lm_model_interaction <- lm(years_of_experience ~ position_group + height + weight + height_weight_interaction, data = train_data1)
+
+# Summary of the model with interaction term
+summary(lm_model_interaction)
+
+# Make predictions on the test set
+predictions_new <- predict(lm_model_interaction, newdata = test_data1)
+
+# Continue with the comparison and RMSE calculation as before
+comparison_new <- data.frame(Actual = test_data1$years_of_experience, Predicted = predictions_new)
+print(head(comparison_new))
+
+# Calculate RMSE
+rmse_new <- sqrt(mean((predictions_new - test_data1$years_of_experience)^2))
+cat("Root Mean Squared Error (RMSE):", rmse_new, "\n")
+
+#so not much difference in RMSE with or without interaction term
+
+#----------------------------------------------------
+# library(randomForest)
+
+# Split the dataset into training (80%) and testing (20%) sets
+# set.seed(123)  # for reproducibility
+# train_indices <- sample(1:nrow(nfl_clean), 0.8 * nrow(nfl_clean))
+# train_data1 <- nfl_clean[train_indices, ]
+# test_data1 <- nfl_clean[-train_indices, ]
+
+# Train the Random Forest model without the interaction term
+rf_model <- randomForest(
+  years_of_experience ~ position_group + height + weight,
+  data = train_data1,
+  ntree = 500,  # Number of trees in the forest
+  mtry = 3,     # Number of variables randomly sampled as candidates at each split
+  importance = TRUE
+)
+
+# Summary of the model
+print(rf_model)
+
+# Make predictions on the test set
+predictions_rf <- predict(rf_model, newdata = test_data1)
+
+# Continue with the comparison and RMSE calculation as before
+comparison_rf <- data.frame(Actual = test_data1$years_of_experience, Predicted = predictions_rf)
+print(head(comparison_rf))
+
+# Calculate RMSE
+rmse_rf <- sqrt(mean((predictions_rf - test_data1$years_of_experience)^2))
+cat("Root Mean Squared Error (RMSE) - Random Forest:", rmse_rf, "\n")
